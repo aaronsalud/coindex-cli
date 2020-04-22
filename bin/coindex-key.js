72e03c8cb9ad0ddb6a9977f8b2dd97d4f49d8087
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2020-04-21 20:16:49 -0400

    Define show command

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -5,4 +5,9 @@ program
 .description('Set API Key -- Get at https://nomics.com')
 .action(()=> console.log('Hello from set'));
 
+program
+.command('show')
+.description('Show API Key')
+.action(()=> console.log('Hello from show'));
+
 program.parse(process.argv);
